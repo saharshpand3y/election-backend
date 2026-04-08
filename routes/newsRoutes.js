c4b52168596fa92e2353e11e895c33d219cda595
Saharsh Pandey <saharshp989@gmail.com>
2026-04-08 14:25:25 +0530

    update

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -7,14 +7,39 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
+    const filter = req.body.filter || "election"; // Get filter from query params instead of body
+    let category, start, end;
+    
+    switch(filter.toLowerCase()) {
+      case "national":
+        category = "indian+foreign+affairs";
+        start = 0;
+        end = 30;
+        break;
+      case "state":
+        category = "indian+states+and+current+affairs";
+        start = 0;
+        end = 30;
+        break;
+      case "economy":
+        category = "indian+economy+and+finances";
+        start = 0;
+        end = 30;
+        break;
+      default:
+        category = "indian+election+and+politics";
+        start = 11;
+        end = 41;
+    }
+    
     const newsResponse = await axios.get(
-      `https://newsapi.org/v2/everything?q=indian+election&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`,
+      `https://newsapi.org/v2/everything?q=${category}&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`,
     );
-    let articles = newsResponse.data.articles.slice(7, 37);
+    let articles = newsResponse.data.articles.slice(start, end);
 
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    // const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     // const analyzedArticles = await Promise.all(
     //   articles.map(async (article) => {
